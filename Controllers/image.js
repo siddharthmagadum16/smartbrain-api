80b52804dd6bb84f1f998e2002e8ec5970e5ac00
Siddharth <siddharthmagadum16@gmail.com>
2020-07-09 13:43:49 +0530

    unable to detect face

--- Controllers/image.js
+++ Controllers/image.js
@@ -1,18 +1,18 @@
 const clarifai=require('clarifai');
 
 const app = new Clarifai.App({
-    apiKey: 'e6958cb4b7d040e7855ea77c36b510ab' 
+    apiKey: 'e6958cb4b7d040e7855ea77c36b510ab'
 });
 
 const ApiCallHandler= (req,res)=>{
     app.models
         .predict('c0c0ac362b03416da06ab3fa36fb58e3',req.body.input)
         .then(data=> res.json(data))
-        .catch(err => res.status(400).json("Unable to detect face"))
-                  
+        .catch(err => res.status(400).json(`Unable to detect face${err}`))
+
 }
 
-const ImageHandler =(req,res,db)=>{ 
+const ImageHandler =(req,res,db)=>{
     const { id: id } = req.body;
     db('users').where({id})
     db('users')
@@ -24,6 +24,6 @@ const ImageHandler =(req,res,db)=>{
 }
 
 module.exports={
-    ImageHandler, ApiCallHandler 
+    ImageHandler, ApiCallHandler
 }
 // e6958cb4b7d040e7855ea77c36b510ab
